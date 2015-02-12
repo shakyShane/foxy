7df28381d017b5732fca35318cd02c3be5820b7d
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-12 17:26:21 +0000

    Tidy example

--- example.js
+++ example.js
@@ -1,14 +1,6 @@
 var http = require("http");
 var app  = require("./")("http://www.bbc.co.uk");
 
-//app.use(function endOfStack(req, res, next) {
-//    console.log("This mw will run last");
-//    next();
-//});
-//
-app.stack.unshift({route: "", handle: function frontOfStack(req, res, next) {
-    next();
-}});
+var server = app.listen(8001);
 
-var server = http.createServer(app);
-server.listen(8002);
+console.log(server.address().port);
