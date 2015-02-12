dd1da6333d512c9fba0623f079836f22321972ed
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-12 10:48:08 +0000

    Add self-signed certs for testing.

---
+++ examples/example.https.js
@@ -0,0 +1,13 @@
+var foxy      = require("./../index");
+var https     = require("https");
+var fs        = require("fs");
+var path      = require("path");
+
+var key = fs.readFileSync(path.resolve(__dirname, "../test/fixtures/certs/server.key"));
+var crt = fs.readFileSync(path.resolve(__dirname, "../test/fixtures/certs/server.cert"));
+
+var server = https.createServer({key: key, cert: crt}, foxy("https://github.com"));
+
+server.listen(8001);
+
+console.log(server.address());
