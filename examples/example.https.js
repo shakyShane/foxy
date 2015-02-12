1fe435284ed0d58330b882a39d8722e82cf88844
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-12 12:22:39 +0000

    Add HTTPS example

--- examples/example.https.js
+++ examples/example.https.js
@@ -6,8 +6,11 @@ var path      = require("path");
 var key = fs.readFileSync(path.resolve(__dirname, "../test/fixtures/certs/server.key"));
 var crt = fs.readFileSync(path.resolve(__dirname, "../test/fixtures/certs/server.cert"));
 
-var server = https.createServer({key: key, cert: crt}, foxy("https://github.com"));
+var server = https.createServer({
+    key: key,
+    cert: crt
+}, foxy("https://github.com"));
 
 server.listen(8001);
 
-console.log(server.address());
+console.log("server running at https://localhost:" + server.address().port);
