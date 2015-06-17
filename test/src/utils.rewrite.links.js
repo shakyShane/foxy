4c94759c92aea891b6b9bceb80bc72f21908111f
Shane Osbourne <shane@wearejh.com>
2015-06-17 10:31:07 +0100

    Added failing test for https://github.com/BrowserSync/browser-sync/issues/676#issuecomment-112733304

--- test/src/utils.rewrite.links.js
+++ test/src/utils.rewrite.links.js
@@ -213,5 +213,13 @@ describe("Rewriting Domains", () => {
             var actual   = input.replace(rewrite.match, rewrite.fn);
             assert.equal(actual, input);
         });
+        it("should not remove trailing slash", () => {
+            var input    = `<a href="http://example.com:1234/foo/">Link 1</a>`;
+            var expected = `<a href="//${proxyUrl}/foo/">Link 1</a>`;
+            var rewrite  = utils.rewriteLinks({hostname: "example.com", port: 1234}, proxyUrl);
+            var actual   = input.replace(rewrite.match, rewrite.fn);
+            console.log(actual);
+            assert.equal(actual, expected);
+        });
     });
 });
